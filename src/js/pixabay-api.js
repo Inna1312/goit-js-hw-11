21fe50b182c958d6cf488a26d5988fdce809e91b
Inna1312 <126244200+Inna1312@users.noreply.github.com>
2023-08-14 22:01:13 +0300

    fix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,19 +3,30 @@ import axios from "axios";
 export class PixabayAPI {
     #BASE_URL = 'https://pixabay.com/api/';
     #API_KEY = '38793454-fa2bc2c38fb343cb826e2dd02';
-    q = null;
+
     page = 1;
+    query = null;
+    per_page = 40;
+
     async fetchPhotos() {
          const searchParams = new URLSearchParams({
              key: this.#API_KEY,
              image_type: 'photo',
              orientation: 'horizontal',
              safesearch: true,
-             per_page: 20,
+             per_page: this.per_page,
              q: this.q,
              page: this.page,
          }
          );
         return await axios.get(`${this.#BASE_URL}?${searchParams}`);
-}
+    }
+    
+     changePage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
 }
