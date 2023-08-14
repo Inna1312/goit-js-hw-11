8c2d12690e99458d8352cb6a34ce84946f5f7fe4
Inna1312 <126244200+Inna1312@users.noreply.github.com>
2023-08-14 10:28:09 +0300

    fix

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export class PixabayAPI {
+    #BASE_URL = 'https://pixabay.com/api/';
+    #API_KEY = '38793454-fa2bc2c38fb343cb826e2dd02';
+    q = null;
+    page = 1;
+    async fetchPhotos() {
+         const searchParams = new URLSearchParams({
+             key: this.#API_KEY,
+             image_type: 'photo',
+             orientation: 'horizontal',
+             safesearch: true,
+             per_page: 40,
+             q: this.q,
+             page: this.page,
+         }
+         );
+        return await axios.get(`${this.#BASE_URL}?${searchParams}`);
+}
+}
