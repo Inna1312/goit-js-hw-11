9364934d55b7e2df8157c78d22ca43916868ef42
Inna1312 <126244200+Inna1312@users.noreply.github.com>
2023-08-15 14:39:40 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,80 +1,95 @@
-import { PixabayAPI } from "./pixabay-api";
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { createMarkup } from "./markup";
 
-const gallery = document.querySelector('.gallery');
-const searchForm = document.querySelector('.search-form');
+import PixabayAPI from './pixabay-api';
+
+const pixabayInstanse = new PixabayAPI();
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
+
+const searchFormEl = document.querySelector('.search-form');
+const inputEl = document.querySelector('input');
+const galleryEl = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
-const target = document.querySelector('.js-guard');
 
-const pixabayAPI = new PixabayAPI();
-const simplelightbox = new SimpleLightbox('.gallery a', {});
+galleryEl.addEventListener('click', onGalleryClick);
 
-const options = {
-  root: null,
-  rootMargin: "400px",
-  threshold: 1.0,
-};
+loadMoreBtn.classList.add('hide');
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
+loadMoreBtn.addEventListener('click', loadMore);
 
-const observer = new IntersectionObserver(handleIntersect, options);
-searchForm.addEventListener('submit', handlerSearchForm);
-
-
-function handlerSearchForm(evt) {
-  evt.preventDefault();
-  target.hidden = true;
-
-  gallery.innerHTML = '';
-  const searchQuery = evt.currentTarget.elements['searchQuery'].value.trim();
-  pixabayAPI.q = searchQuery;
-  pixabayAPI.page = 1;
-  searchPhotos();
+function onSearchFormSubmit(event) {
+  event.preventDefault();
+  if (!inputEl.value) {
+    return;
+  }
+  galleryEl.innerHTML = '';
+  pixabayInstanse.resetPage();
+  loadMoreBtn.classList.add('hide');
+  pixabayInstanse.query = inputEl.value.trim();
+  pixabayInstanse
+    .fetchImages()
+    .then(({ data: { totalHits, hits } }) => {
+      if (!totalHits) {
+        Notiflix.Notify.failure(
+          `Sorry, there are no images matching your search query. Please try again.`
+        );
+      } else {
+        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+      }
+      if (totalHits > pixabayInstanse.per_page) {
+        loadMoreBtn.classList.remove('hide');
+      }
+      pixabayInstanse.changePage();
+      return createMarkup(hits);
+    })
+    .then(renderMarkup)
+    .catch(error => console.log(error));
+  // event.target.reset();
 }
 
-async function searchPhotos() {
-  try {
-    const { data } = await pixabayAPI.fetchPhotos();
-    if (data.hits.length < 1) {
-      Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-      return;
-    }
+function loadMore() {
+  pixabayInstanse
+    .fetchImages()
+    .then(({ data: { hits } }) => {
+      pixabayInstanse.changePage();
+      if (hits.length < pixabayInstanse.per_page) {
+        loadMoreBtn.classList.add('hide');
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
 
-    Notify.success(`Hooray! We found ${data.totalHits} images.`);
-    gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
-    target.hidden = false;
-    observer.observe(target);
-    simplelightbox.refresh();
-  
-  } catch (error) {
-    console.log(error);
-    
-  };
+      return createMarkup(hits);
+    })
+    .then(markup => renderMarkup(markup))
+    .then(scrollToUp)
+    .catch(error => console.log(error));
 }
-
-function handleIntersect(evt) {
-  pixabayAPI.page += 1;
-  if (evt[0].isIntersecting) {
-    searchMorePhotos(); 
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
   }
 }
 
- 
-async function searchMorePhotos() {
-  try {
-    const result = pixabayAPI.page;
-    const { data } = await pixabayAPI.fetchPhotos();
-    gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits)); 
-    if (result >= data.totalHits) {
-      observer.unobserve(target);
-      Notify.failure("We're sorry, but you've reached the end of search results.");
-      return;
-    };
-   
-    simplelightbox.refresh();
-  } catch (error) {
-    
-  }
+function renderMarkup(murkup) {
+  galleryEl.insertAdjacentHTML('beforeend', murkup);
+  lightbox.refresh();
+}
 
+function scrollToUp() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,28 +1,30 @@
-import axios from "axios";
 
-export class PixabayAPI {
-    #BASE_URL = 'https://pixabay.com/api/';
-    #API_KEY = '38793454-fa2bc2c38fb343cb826e2dd02';
+import axios from 'axios';
 
-    page = 1;
-    query = null;
-    per_page = 40;
+export default class PixabayAPI {
+  #BASE_KEY = '38793454-fa2bc2c38fb343cb826e2dd02';
+  #BASE_URL = 'https://pixabay.com/api/';
 
-    async fetchPhotos() {
-         const searchParams = new URLSearchParams({
-             key: this.#API_KEY,
-             image_type: 'photo',
-             orientation: 'horizontal',
-             safesearch: true,
-             per_page: this.per_page,
-             q: this.q,
-             page: this.page,
-         }
-         );
-        return await axios.get(`${this.#BASE_URL}?${searchParams}`);
-    }
-    
-     changePage() {
+  page = 1;
+  query = null;
+  per_page = 40;
+
+  async fetchImages() {
+    const baseSearchParams = new URLSearchParams({
+      key: this.#BASE_KEY,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: this.per_page,
+      page: this.page,
+      q: this.query,
+    });
+
+    const data = await axios.get(`${this.#BASE_URL}?${baseSearchParams}`);
+    return data;
+  }
+
+  changePage() {
     this.page += 1;
   }
 
